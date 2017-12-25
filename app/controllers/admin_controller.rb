0745da530f7f881e3a7f0cb301883b5b7a229fab
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2017-12-25 13:45:52 +0700

    Added admin dashboard and it's template

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,16 +1,31 @@
 class AdminController < ApplicationController
-  def dashboard
-  end
+	before_action :authenticate_user!
+	before_action :check_admin
 
-  def restore
-  end
+	def dashboard
+		@orders = Order.all 
+	end
 
-  def item
-  end
+	def restore
+	end
 
-  def user
-  end
+	def item
+		@items = Item.all
+	end
 
-  def history
-  end
+	def user
+		@users = User.all
+	end
+
+	def history
+		@orders = Order.all
+	end
+
+	private
+
+	def check_admin
+		unless current_user.has_role? :admin
+			redirect_to root_path
+		end
+	end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,17 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( 
+admin/AdminLTE.min.scss
+admin/_all-skins.min.scss
+admin/bootstrap.min.scss
+plugin/morris.js/morris.scss
+admin/adminlte.min.js
+admin/pages/dashboard.js
+admin/demo.js
+plugin/raphael/raphael.min.js
+plugin/morris.js/morris.min.js
+plugin/jquery-slimscroll/jquery.slimscroll.min.js
+plugin/fastclick/fastclick.js
 admin.js 
 admin.css 
 pages.coffee
